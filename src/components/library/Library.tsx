0e2fb4fcbb56bf3bccd6cbab1270077b69a90752
fercoopat <fercoopat@mail.com>
2023-10-06 21:57:01 -0400

    Recount of available books added, the number of books in the reading list and in the available books is synchronized

--- src/components/library/Library.tsx
+++ src/components/library/Library.tsx
@@ -1,19 +1,25 @@
+import { useMemo } from 'react';
 import { Book, GenderSelector } from '..';
 import { GENDER } from '../../constants';
 import { useFilteredBooks, useLibraryContext } from '../../hooks';
 import styles from './library.module.css';
 
 function Library() {
-  const { books } = useLibraryContext();
+  const { books, readingList } = useLibraryContext();
   const { filteredBooks, selectedGender, changeGenderHandler } =
     useFilteredBooks();
+
+  const booksAvailable = useMemo(
+    () => books.length - readingList.length,
+    [books, readingList]
+  );
 
   if (filteredBooks.length < 1) return <h1>No books to show! :(</h1>;
 
   return (
     <>
       <header>
-        <h1>Total of books: {books.length}</h1>
+        <h1>Total of books available: {booksAvailable}</h1>
         {selectedGender !== GENDER.ALL && (
           <h1>
             Books of {selectedGender}: {filteredBooks.length}
